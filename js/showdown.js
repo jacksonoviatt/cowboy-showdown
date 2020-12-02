447d4f1b0ec2be83e16b11398dbd65cda52d85ca
Ramona <jackson.l.ehlert@gmail.com>
2020-12-01 21:42:36 -0700

    small typos fixed again but this time i saved my document before push

--- js/showdown.js
+++ js/showdown.js
@@ -34,25 +34,26 @@ document.querySelector('.cpu img').src = cpu.cowboy.image;
 
 
 
-function showdown(){
-    if(playerHp > 0 && cpuHp > 0){
+function showdown() {
+    if (playerHp > 0 && cpuHp > 0) {
         cpuHp = (cpuHp - playerShot);
         playerHp = (playerHp - cpuShot);
         document.querySelector('.player .health').innerHTML = playerHp;
         document.querySelector('.cpu .health').innerHTML = cpuHp;
-    }else if (cpuHp <= 0 && playerHp <= 0){
+    } else if (cpuHp <= 0 && playerHp <= 0) {
         document.querySelector(".statement").innerHTML = "Draw";
-        }else if (cpuHp <= 0){
-            document.querySelector(".statement").innerHTML = "You win";
-        }else if (playerHp <= 0){
-                document.querySelector(".statement").innerHTML = "You lose";
-        }else {
-            null
-} }
+    } else if (cpuHp <= 0) {
+        document.querySelector(".statement").innerHTML = "You win";
+    } else if (playerHp <= 0) {
+        document.querySelector(".statement").innerHTML = "You lose";
+    } else {
+        null
+    }
+}
 
 let shootButton = document.querySelector('.game-button');
 shootButton.innerHTML = "Shot";
-shootButton.addEventListener('click', function(){
+shootButton.addEventListener('click', function () {
     showdown();
 });
 
